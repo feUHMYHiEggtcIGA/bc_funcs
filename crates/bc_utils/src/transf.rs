0120f5ae50b75138de41f1e1c177d0887018edce
name lastname <you@example.com>
2025-03-21 16:20:35 +0700

    //

--- crates/bc_utils/src/transf.rs
+++ crates/bc_utils/src/transf.rs
@@ -1,31 +1,35 @@
 use std::borrow::Borrow;
 
-use num_traits::Float;
+use num_traits::{Float, Num, ToPrimitive};
 
 
-pub fn vec_positive<'a, I, T>(
-    iter_: I,
-) -> Vec<&'a T> 
+pub fn vec_positive<'a, T, V>(
+    slice: &'a[V],
+) -> Vec<&'a T>
 where 
-    I: Iterator<Item = &'a T>,
     T: Float,
     T: 'a,
+    V: Borrow<T>,
 {
-    iter_
-        .filter(|v| **v > T::zero())
+    slice
+        .iter()
+        .filter(|v| (**v).borrow() > &T::zero())
+        .map(|v| v.borrow())
         .collect()
 }
 
-pub fn vec_negative<'a, I, T>(
-    iter_: I,
-) -> Vec<&'a T> 
+pub fn vec_negative<'a, T, V>(
+    slice: &'a[V],
+) -> Vec<&'a T>
 where 
-    I: Iterator<Item = &'a T>,
     T: Float,
     T: 'a,
+    V: Borrow<T>,
 {
-    iter_
-        .filter(|v| **v < T::zero())
+    slice
+        .iter()
+        .filter(|v| (**v).borrow() < &T::zero())
+        .map(|v| v.borrow())
         .collect()
 }
 
@@ -36,8 +40,9 @@ pub fn lstrip(
     let mut cut_index: usize = 0;
 
     for c in s
-    .chars()
-    .enumerate() {
+        .chars()
+        .enumerate() 
+    {
         if c.1 == cut_before {
             cut_index = c.0;
             break;
@@ -53,112 +58,117 @@ pub fn rstrip(
     let mut cut_index: usize = 0;
 
     for c in s
-    .chars()
-    .rev()
-    .enumerate() {
+        .chars()
+        .rev()
+        .enumerate() 
+    {
         if c.1 == cut_before {
             cut_index = c.0;
             break;
         }
     }
-    &s[cut_index..]
+    &s[..s.len() - cut_index]
 }
 
 pub fn nz<T, V>(
-    num: V, 
+    num: V,
     exc_value: V,
 ) -> V
 where 
     T: Float,
     V: Borrow<T>,
 {
-    if num.borrow().is_nan() {
-        exc_value
-    } else {
-        num
-    }
+    if num.borrow().is_nan() { exc_value } else { num }
 }
 
-pub fn nz_vec<T, F>(
-    iter_: T,
-    exc_value: F,
-) -> Vec<F>
+pub fn vec_nz<T, V>(
+    slice: &[V],
+    exc_value: V,
+) -> Vec<V>
 where 
-    T: Iterator<Item = F>,
-    F: Float,
+    T: Float,
+    V: Borrow<T>,
+    V: Copy,
 {
-    iter_
-        .map(|num| nz(num, exc_value))
+    slice
+        .iter()
+        .map(|num| nz(*num, exc_value))
         .collect()
 }
 
-pub fn normalize<'a, T, F>(
-    iter_: T,
-    to_normalize: &F,
-    min_new: F,
-    max_new: F,
-) -> F
+pub fn normalize<'a, T, V>(
+    slice: &[V],
+    to_normalize: V,
+    min_new: &T,
+    max_new: &T,
+) -> T
 where 
-    T: Iterator<Item = &'a F>,
-    F: 'a,
-    F: Float,
-    F: std::ops::Sub<Output = F>,
-    F: std::ops::Div<Output = F>,
-    F: std::ops::Mul<Output = F>,
-    F: std::ops::Add<Output = F>,
+    T: 'a,
+    T: Float,
+    T: std::ops::Sub<Output = T>,
+    T: std::ops::Div<Output = T>,
+    T: std::ops::Mul<Output = T>,
+    T: std::ops::Add<Output = T>,
+    V: Borrow<T>,
+    V: std::fmt::Debug + std::fmt::Display,
 {
-    let mut min_historic = &F::nan();
-    let mut max_historic = &F::nan();
+    let mut min_historic = T::infinity();
+    let mut max_historic = -T::infinity();
 
-    for num in iter_ {
-        min_historic = if num < &min_historic {num} else {min_historic};
-        max_historic = if num > &max_historic {num} else {min_historic};
+    for num in slice {
+        min_historic = if num.borrow() < &min_historic {*num.borrow()} else {min_historic};
+        max_historic = if num.borrow() > &max_historic {*num.borrow()} else {max_historic};
     };
-    (*to_normalize - *min_historic)
-        / (*max_historic - *min_historic)
-        * (max_new - min_new)
-        + min_new
+    (*to_normalize.borrow() - min_historic)
+        / (max_historic - min_historic)
+        * (*max_new - *min_new)
+        + *min_new
 }
 
-pub fn dz<F: Float>(
-    num: F,
-) -> F {
-    if num == F::zero() {
-        F::from(1e-10).unwrap()
-    }
-    else {
-        num
-    }
-}
-
-pub fn vec_drop_nan<F: Float>(
-    vec: Vec<F>,
-) -> Vec<F> {
-    vec.into_iter()
-       .filter(|&x|!x.is_nan())
-       .collect()
-}
-
-pub fn abs<T>(
-    num: &T,
+pub fn dz<T>(
+    num: T,
 ) -> T 
-where
+where 
     T: Float,
 {
-    if *num < T::zero() {
-        -*num
-    } else {
-        *num
-    }
+    if num == T::zero() { T::from(1e-10).unwrap() } else { num }
 }
 
-pub fn avg<'a, T, I>(
+pub fn coll_drop_nan<T, V, C>(
+    vec: &[V],
+) -> C
+where 
+    T: Float,
+    V: Borrow<T>,
+    V: Copy,
+    C: FromIterator<V>,
+{
+    vec
+        .iter()
+        .filter(|x| !(**x).borrow().is_nan())
+        .copied()
+        .collect()
+}
+
+pub fn abs<T, V>(
+    num: V,
+) -> T 
+where
+    T: Num,
+    T: PartialOrd,
+    T: std::ops::Neg<Output = T>,
+    T: Copy,
+    V: Borrow<T>,
+{
+    if num.borrow() < &T::zero() { -*num.borrow() } else { *num.borrow() }
+}
+
+pub fn avg<T, I>(
     iter_: I,
 ) -> T 
 where 
     T: Float,
-    T: 'a,
-    I: Iterator<Item = &'a T>,
+    I: Iterator<Item = T>,
     T: std::ops::AddAssign<T>,
 {
     let mut count = 0;
@@ -166,83 +176,87 @@ where
 
     for (i, el) in iter_.enumerate() {
         count = i;
-        sum += *el;
+        sum += el;
     }
     sum / T::from(count + 1).unwrap()
 }
 
-pub fn vec1_roll<T, I>(
-    iter_: I,
-    shift: i8,
-) -> Vec<T>
-where 
-    I: Iterator<Item = T>,
+pub fn roll_slice1<'a, T>(
+    v: &'a mut [T],
+    shift: &i8,
+)
 {    
-    let shift_usize = shift.abs() as usize;
-    let mut res: Vec<T> = iter_.collect();
+    let shift_usize = (*shift)
+        .abs()
+        .to_usize()
+        .unwrap();
     
-    if shift > 0 {
-        res.rotate_right(shift_usize);
-    } else if shift < 0 {
-        res.rotate_left(shift_usize);
+    match shift.cmp(&0) {
+        std::cmp::Ordering::Greater => v.rotate_right(shift_usize),
+        std::cmp::Ordering::Less => v.rotate_left(shift_usize),
+        std::cmp::Ordering::Equal => {}
     }
-    res
 }
 
-pub fn vec1_roll_replace_el<'a, T, I>(
-    iter_: I,
-    len_iter: &usize,
-    shift: i8,
-    to_replace: T,
-) -> Vec<T>
+pub fn coll1_roll_replace_el<'a, T, V, C>(
+    slice: &mut [V],
+    shift: &i8,
+    to_replace: V,
+) -> C
 where 
     T: 'a,
-    T: Copy,
-    I: Iterator<Item = T>,
+    V: Borrow<T>,
+    V: Copy,
+    C: FromIterator<V>,
 {
-    let res = vec1_roll(iter_, shift);
-    let shift_usize = shift.abs() as usize;
+    let len = slice.len();
+    roll_slice1(slice, shift);
+    let iter_ = slice.iter();
+    let shift_usize = (*shift)
+        .abs()
+        .to_usize()
+        .unwrap();
 
-    if shift > 0 {
-        let num_need = shift_usize;
-        res
-            .into_iter()
-            .enumerate()
-            .map(|(i, v)| {
-                if i <= num_need {
-                    to_replace
-                } else {
-                    v
-                }
-            })
-            .collect()
-    } else if shift < 0 {
-        let num_need = (*len_iter as i8 + shift) as usize;
-        res
-            .into_iter()
-            .enumerate()
-            .map(|(i, v)| {
-                if i >= num_need {
-                    to_replace
-                } else {
-                    v
-                }
-            })
-            .collect()
-    } else {
-        res
+    match shift.cmp(&0){
+        std::cmp::Ordering::Greater => {
+            let num_need = shift_usize;
+            iter_
+                .enumerate()
+                .map(|(i, v)| {
+                    if i <= num_need {
+                        to_replace
+                    } else {
+                        *v
+                    }
+                })
+                .collect()
+        }
+        std::cmp::Ordering::Less => {
+            let num_need = (len as i8 + shift) as usize;
+            iter_
+                .enumerate()
+                .map(|(i, v)| {
+                    if i >= num_need {
+                        to_replace
+                    } else {
+                        *v
+                    }
+                })
+                .collect()
+        }
+        std::cmp::Ordering::Equal => iter_.copied().collect()
     }
 }
 
 pub fn round_float<T, V>(
     num: V,
-    precision: usize,
+    precision: &usize,
 ) -> T
 where 
     T: Float,
     V: Borrow<T>,
     V: Copy,
 {
-    let mult = T::from(10.0.powi(precision as i32)).unwrap();
+    let mult = T::from(10.0.powi(*precision as i32)).unwrap();
     (*num.borrow() * mult).round() / mult
 }
--- crates/bc_utils/tests/unit/create.rs
+++ crates/bc_utils/tests/unit/create.rs
@@ -3,13 +3,14 @@ use bc_utils::create::*;
 #[test]
 fn sign_1() {
     assert_eq!(1.0, sign(5.0),);
-    assert_eq!(-1.0, sign(-5.0),);
-    assert_eq!(0.0, sign(0.0),);
 }
 
 #[test]
 fn sign_2() {
-    assert_eq!(1.0, sign(&5.0),);
-    assert_eq!(-1.0, sign(&-5.0),);
-    assert_eq!(0.0, sign(&0.0),);
+    assert_eq!(-1.0, sign(-5.0),);
+}
+
+#[test]
+fn sign_3() {
+    assert_eq!(0.0, sign(0.0),);
 }
