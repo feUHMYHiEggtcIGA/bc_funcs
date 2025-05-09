500e5031d60563f3c252ae4f60d83d8c55d6aade
name lastname <you@example.com>
2025-05-09 12:16:11 +0300

    bc_signals: mm fix

--- crates/bc_signals/src/train/mm.rs
+++ crates/bc_signals/src/train/mm.rs
@@ -8,6 +8,7 @@ use bc_utils_lg::types::structures::SRC;
 
 pub fn mm_coll(
     src: &[SRC<f64>],
+    key: &str,
     window: &usize,
     min_distance: &usize,
     tp_th: &f64,
@@ -25,17 +26,20 @@ pub fn mm_coll(
             .iter()
             .enumerate()
             .fold(
-                ((0, f64::INFINITY), (0, f64::NEG_INFINITY),),
-                |init, v| (
-                    match init.0.1.partial_cmp(&v.1["low"]) {
-                        Some(Ordering::Greater) => (v.0 + i, v.1["low"]),
-                        _ => init.0,
-                    },
-                    match init.1.1.partial_cmp(&v.1["high"]) {
-                        Some(Ordering::Less) => (v.0 + i, v.1["high"]),
-                        _ => init.1,
-                    }
-                )
+                ((i, src[i][key]), (i, src[i][key]),),
+                |init, v| {
+                    let vkey = v.1[key];
+                    (
+                        match vkey.partial_cmp(&init.0.1) {
+                            Some(Ordering::Less) => (v.0 + i, vkey),
+                            _ => init.0,
+                        },
+                        match vkey.partial_cmp(&init.1.1) {
+                            Some(Ordering::Greater) => (v.0 + i, vkey),
+                            _ => init.1,
+                        },
+                    )
+                }
             );
         let index_min = min_by_key(&min, &max, |v| v.0);
         let index_max = max_by_key(&min, &max, |v| v.0);
@@ -45,11 +49,11 @@ pub fn mm_coll(
             && &range_ <= tp_limit
         {
             if index_min.1 < index_max.1 {
-                entrances[index_min.0] = -1.0;
-                exits[index_max.0] = 1.0;
-            } else {
                 entrances[index_min.0] = 1.0;
                 exits[index_max.0] = -1.0;
+            } else {
+                entrances[index_min.0] = -1.0;
+                exits[index_max.0] = 1.0;
             }
         }
     }
